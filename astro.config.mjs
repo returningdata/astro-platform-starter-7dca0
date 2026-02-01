c09950ef76adf01473cb1f8124c3e82dd23f08ba
returningdata <71713395+returningdata@users.noreply.github.com>
2026-01-31 19:00:10 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server",
   adapter: netlify(),
+
+  // Netlify adapter currently doesn't support Astro "assets"
+  assets: false,
 });
